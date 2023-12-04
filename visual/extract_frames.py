67ff8e29b54704a74ce243a7cb1b7e73d90454e5
Koisu <koisuspotify@gmail.com>
2023-12-04 08:41:25 -0700

    Got function to extract frames

--- visual/extract_frames.py
+++ visual/extract_frames.py
@@ -1,10 +1,42 @@
-import cv2
-vidcap = cv2.VideoCapture('Bad_Apple.mp4')
-success,image = vidcap.read()
-print(success)
-count = 0
-while success:
-  cv2.imwrite("frames/frame%d.jpg" % count, image)     # save frame as JPEG file      
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
+# Importing all necessary libraries 
+import cv2 
+import os 
+
+# Read the video from specified path 
+cam = cv2.VideoCapture("visual\Bad_Apple.mp4") 
+
+try: 
+	
+	# creating a folder named data 
+	if not os.path.exists('data'): 
+		os.makedirs('data') 
+
+# if not created then raise error 
+except OSError: 
+	print ('Error: Creating directory of data') 
+
+# frame 
+currentframe = 0
+
+while(True): 
+	
+	# reading from frame 
+	ret,frame = cam.read() 
+
+	if ret: 
+		# if video is still left continue creating images 
+		name = './data/frame' + str(currentframe) + '.jpg'
+		print ('Creating...' + name) 
+
+		# writing the extracted images 
+		cv2.imwrite(name, frame) 
+
+		# increasing counter so that it will 
+		# show how many frames are created 
+		currentframe += 1
+	else: 
+		break
+
+# Release all space and windows once done 
+cam.release() 
+cv2.destroyAllWindows() 
